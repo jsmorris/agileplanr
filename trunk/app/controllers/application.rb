3f781227c79bd331064fce6d4f1d758efea049c1
jsmorris <jsmorris@a3bf96bc-a71f-0410-ac3c-1910bb5774f4>
2006-10-16 03:59:33 +0000

    added two default users:  admin and guest added the application layout removed the other controller layouts...not needed added some various routs added several new rights and their association to the admin role

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -4,12 +4,13 @@ class ApplicationController < ActionController::Base
 
   before_filter :check_authentication,
                 :check_authorization,
-                :except => [:signin_form, :signin]
+                :except => [:signin]
   
   def check_authentication
     unless session[:user]
       session[:intended_action] = action_name
       redirect_to :controller => "admin", :action => "signin"
+      return false
     end
   end
   
@@ -21,9 +22,10 @@ class ApplicationController < ActionController::Base
         }
       }
       flash[:notice] = "You are not authorized to view the page you requested"
-      reqest.env["HTTP_REFERER"] ? (redirect_to :back) : (redirect_to home_url)
+      request.env["HTTP_REFERER"] ? (redirect_to :back) : (redirect_to home_url)
       return false
     end
+    flash[:notice] = nil
   end
   
 end
--- trunk/db/migrate/015_add_rights.rb
+++ trunk/db/migrate/015_add_rights.rb
@@ -1,17 +1,39 @@
 class AddRights < ActiveRecord::Migration
 
   def self.up
-    Right.create(:name => "New Project", :controller => "project", :action => "new")
-    Right.create(:name => "Edit Project", :controller => "project", :action => "edit")
-    Right.create(:name => "List Project", :controller => "project", :action => "list")
-    Right.create(:name => "Show Project", :controller => "project", :action => "show")
+    # Project rights
+    Right.create(:name => "New Project", :controller => "projects", :action => "new")
+    Right.create(:name => "Create Project", :controller => "projects", :action => "create")
+    Right.create(:name => "Edit Project", :controller => "projects", :action => "edit")
+    Right.create(:name => "Destroy Project", :controller => "projects", :action => "destroy")
+    Right.create(:name => "List Projects", :controller => "projects", :action => "list")
+    Right.create(:name => "Show Project", :controller => "projects", :action => "show")
+
+    # Iteration rights
+    Right.create(:name => "New Iteration", :controller => "iterations", :action => "new")
+    Right.create(:name => "Create Iteration", :controller => "iterations", :action => "create")
+    Right.create(:name => "Edit Iteration", :controller => "iterations", :action => "edit")
+    Right.create(:name => "Destroy Iteration", :controller => "iterations", :action => "destroy")
+    Right.create(:name => "List Iterations", :controller => "iterations", :action => "list")
+    Right.create(:name => "Show Iteration", :controller => "iterations", :action => "show")
   end
 
   def self.down
+    # Project rights
     Right.delete(Right.find(:first, :conditions => [ "name = ?", "New Project" ]).id)
+    Right.delete(Right.find(:first, :conditions => [ "name = ?", "Create Project" ]).id)
     Right.delete(Right.find(:first, :conditions => [ "name = ?", "Edit Project" ]).id)
-    Right.delete(Right.find(:first, :conditions => [ "name = ?", "List Project" ]).id)
+    Right.delete(Right.find(:first, :conditions => [ "name = ?", "Destroy Project" ]).id)
+    Right.delete(Right.find(:first, :conditions => [ "name = ?", "List Projects" ]).id)
     Right.delete(Right.find(:first, :conditions => [ "name = ?", "Show Project" ]).id)
+
+    # Iteration rights
+    Right.delete(Right.find(:first, :conditions => [ "name = ?", "New Iteration" ]).id)
+    Right.delete(Right.find(:first, :conditions => [ "name = ?", "Create Iteration" ]).id)
+    Right.delete(Right.find(:first, :conditions => [ "name = ?", "Edit Iteration" ]).id)
+    Right.delete(Right.find(:first, :conditions => [ "name = ?", "Destroy Iteration" ]).id)
+    Right.delete(Right.find(:first, :conditions => [ "name = ?", "List Iterations" ]).id)
+    Right.delete(Right.find(:first, :conditions => [ "name = ?", "Show Iteration" ]).id)
   end
   
 end
